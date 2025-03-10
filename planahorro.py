1d1493f2ecc6c5212becac71fbf87c5891a9ba60
Bryan-MurilloV <bryanmurillo131206@gmail.com>
2025-03-09 22:40:41 -0600

    Add the name input to the window

--- planahorro.py
+++ planahorro.py
@@ -12,6 +12,14 @@ def main(page: ft.Page):
 
     page.theme_mode = ft.ThemeMode.LIGHT
 
+    txt_preview = ft.Text(value="Ingresa los datos completos",
+                          size=14, weight=ft.FontWeight.BOLD)
+
+    def actualizar_preview(e):
+        txt_preview.value = f"""
+        Nombre: {nombre_input.value.title()}"""
+        page.update()
+
     def actualizar_tema(e):
         if tema_switch.value:
             page.theme_mode = ft.ThemeMode.DARK
@@ -22,9 +30,12 @@ def main(page: ft.Page):
             titulo.color = ft.Colors.PINK_300
             page.update()
 
+    nombre_input = ft.TextField(
+        label="Nombre:", border_radius=8, on_change=actualizar_preview)
+
     tema_switch = ft.Switch(label="Modo Oscuro", on_change=actualizar_tema)
 
-    page.add(titulo, tema_switch)
+    page.add(titulo, tema_switch, nombre_input, txt_preview)
 
 
 ft.app(main)
